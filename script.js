a5eefc84588288a47f63f58731fc9b3e2c9c737c
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2025-04-09 18:55:42 +0700

    commit pertama address book

---
+++ script.js
@@ -0,0 +1,11 @@
+const contact = {
+  name: "Muhammad Rizki",
+  email: "muhammadrizki05@gmail.com",
+  phone: "+6212345678",
+};
+
+const contact1 = {
+  name: "Monica",
+  email: "monica@task.com",
+  phone: "+6212131424",
+};
