524313119ed4ce7baebe36c965eaa90a1879ab8b
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2025-04-09 19:49:02 +0700

    move contacts to array

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const contact = {
-  name: "Muhammad Rizki",
-  email: "muhammadrizki05@gmail.com",
-  phone: "+6212345678",
-};
+const contacts = [
+  {
+    id: 1,
+    name: "Muhammad Rizki",
+    email: "muhammadrizki05@gmail.com",
+    phone: "+6212345678",
+    isLive: true,
+  },
 
-const contact1 = {
-  name: "Monica",
-  email: "monica@task.com",
-  phone: "+6212131424",
-};
+  {
+    id: 2,
+    name: "Monica",
+    email: "monica@example.com",
+    phone: "+6212345678",
+    isLive: true,
+  },
+
+  {
+    id: 3,
+    name: "Shafa",
+    email: "shafana@example.co.id",
+    phone: "+6253536737",
+    isActive: false,
+  },
+];
+
+console.log(contacts);
